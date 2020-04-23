d32fe12f292f257bb7ffa3fb4bb170d7e74b6919
ktranfullerton2000 <42751361+ktranfullerton2000@users.noreply.github.com>
2020-04-22 21:41:21 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+
 #include "tree.h"
 #include "utils.h"
 #include "hashtable.h"
+
 int main(int argc, const char* argv[]) {
-  
-  //exerise 6.2 attempt
-
-//wordFreq(argc,argv);
-
-
-  // exercise 6.3
-
   tree* t = tree_from_file(argc, argv);
-  if (t != NULL) { tree_test(t);  tree_delete(t);  return 0; }
+  if (t == NULL){ t = tree_from_stream(fgets, stdin);}
   
-  char buf[BUFSIZ];
-  char delims[] = " .,;?!\'\"()\n";
-  
-  int size = 0;
-  int linenum = 0;
-  memset(buf, 0, sizeof(buf));
-  
-  t = tree_create();
-
-  while (fgets(buf, BUFSIZ, stdin)) {
-    char* word = strtok(buf, delims);
-    if(word == NULL) linenum++;
-    
-    if(isNoise(word)==0){
-	tree_add(t, word);
-	++size;}
-    
-    while ((word = strtok(NULL, delims)) != NULL) {
-      if(isNoise(word)==0){
-	  tree_add(t, word);
-       ++size;}
-    }
-  }
-  printf("%d words added...\n", size);
-
-  tree_test(t);
+  //6.2, 6.3
+  tree_test(t); 
   tree_delete(t);
+
+  //6.4 uncompleted
 
   //6.5
   
-  printf("\n\nUNDEF TEST-----------------------\n");
-
+  printf("\n\nUNDEF TEST =================================================\n\n");
   undef_test();
-  printf("END UNDEF TEST-----------------------\n");
-  
-
+  printf("\nFINISHED UNDEF TEST ========================================\n");
 
   return 0;
 }
